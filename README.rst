e1fff9a245cd3dfec41c8c4c343f71f56b6377e0
ryuzi <ryuzi.matsuo@gmail.com>
2012-10-22 23:32:11 +0900

    先日作成したプログラムのインポートその他もろもろ
    * 設定ファイルの外部ファイル化をしました
    * settings-sample.py を追加しました
    * 終了時にショートメッセージを表示するようにしました
    * README.rstに簡単な説明を追加しました

---
+++ README.rst
@@ -0,0 +1,16 @@
+利用方法
+========
+
+# https://dev.twitter.com/apps でCONSUMER_KEY/CONSUMER_SECRET/ACCESS_TOKEN/ACCESS_TOKEN_SECRETを取得します。
+# settings-sample.py を settings.py としてコピーします。
+# settings.py に、取得したtwitter情報を設定します。
+
+参考URL
+=======
+
+- http://www.python.jp/doc/release/library/cmd.html
+- https://dev.twitter.com/apps
+- https://dev.twitter.com/docs/api/1.1
+- https://github.com/tweepy/tweepy
+- http://packages.python.org/tweepy/html/index.html
+
---
+++ pytwitter-cui.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import sys
+import random
+from cmd import Cmd
+import tweepy
+
+from settings import *
+
+class TwitterClient(Cmd):
+    def __init__(self):
+        Cmd.__init__(self)
+        self.prompt = PROMPT
+        self.key = ACCESS_TOKEN
+        self.secret = ACCESS_TOKEN_SECRET
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(self.key, self.secret)
+        self.api = tweepy.API(auth)
+        self.auth = auth
+        print "%s logged in" % self.api.me().screen_name
+
+    def do_EOF(self, parameters):
+        print
+        print random.choice(EXIT_MESSAGES)
+        sys.exit()
+
+    def emptyline(self):
+        pass
+
+    def do_timeline(self, count=30):
+        timeline = self.api.home_timeline(count=count)
+        timeline.reverse()
+        for status in timeline:
+            print STATUS_TEMPLATE % (status.created_at, status.user.screen_name, status.text)
+
+    def do_mentions(self, count=30):
+        mentions = self.api.mentions(count=count)
+        mentions.reverse()
+        for status in mentions:
+            print STATUS_TEMPLATE % (status.created_at, status.user.screen_name, status.text)
+
+
+if __name__ == "__main__":
+    client = TwitterClient()
+    try:
+        client.cmdloop()
+    except KeyboardInterrupt:
+        client.onecmd("EOF")
+
